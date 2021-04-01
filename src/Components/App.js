7a87888f24e28dff7eaed774e4d082c9a32a674d
zoo0190 <66614341+zoo0190@users.noreply.github.com>
2021-04-01 15:20:33 +0900

    second commit

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import Router from 'Components/Router';
-import Header from 'Components/Header';
+import GlobalStyles from './GlobalStyles';
 
 class App extends Component {
   render() {
     return (
       <>
-        <Header />
         <Router />
+        <GlobalStyles />
       </>
     );
   }
--- src/Components/Router.js
+++ src/Components/Router.js
@@ -1,3 +1,4 @@
+/* eslint-disable import/no-anonymous-default-export */
 import React from 'react';
 import {
   BrowserRouter as Router,
@@ -8,9 +9,11 @@ import {
 import Home from 'Routes/Home';
 import TV from 'Routes/TV';
 import Search from 'Routes/Search';
+import Header from './Header';
 
 export default () => (
   <Router>
+    <Header />
     <Switch>
       <Route path="/" exact component={Home} />
       <Route path="/tv" exact component={TV} />
